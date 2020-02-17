32a432c45192226c1b871080766a4d4b855c1a76
Danny <mtme13@hotmail.com>
2020-02-17 17:07:23 +0000

    Use @hostbinding to execute child animations on route changes

--- src/app/animations/element-animations.ts
+++ src/app/animations/element-animations.ts
@@ -18,16 +18,27 @@ export const fadeUp = animation([
   // start entering element, invisible and down 15px
   // after 1s, animate to become visible and move up 15px
   query(".child", [
-    style({ opacity: "0", transform: "translateY(-15px)" }),
-    stagger("1s", animate("1s ease-in",
+    style({ opacity: "0", transform: "translateY(30px)" }),
+    stagger("0.4s", animate("0.5s ease-in",
       style({ opacity: "1", transform: "translateY(0px)" })))
   ], { optional: true }),
   // make elements visble with 0.3s delay between each element
   // query(":enter", animateChild(), { optional: true }),
-
-
-  query(":leave", animate("1s ease-in",
-    style({ opacity: "0" })
-  ), { optional: true }),
-  // query(":leave", animateChild(), { optional: true }),
+  query(".child", [
+    animate(1000, style("*"))
+  ])
+]);
+export const fadeDown = animation([
+  // start entering element, invisible and down 15px
+  // after 1s, animate to become visible and move up 15px
+  query(".child", [
+    style({ opacity: "1", transform: "translateY(0px)" }),
+    stagger("-0.4s", animate("0.5s ease-in",
+      style({ opacity: "0", transform: "translateY(30px)" })))
+  ], { optional: true }),
+  // make elements visble with 0.3s delay between each element
+  // query(":enter", animateChild(), { optional: true }),
+  // query(".child", [
+  //   animate(1000, style("*"))
+  // ])
 ]);
--- src/app/pages/page-one/page-one.component.ts
+++ src/app/pages/page-one/page-one.component.ts
@@ -1,12 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-import { fadeUp } from "../../animations/element-animations";
+import { Component, OnInit, HostBinding } from '@angular/core';
+import { fadeUp, fadeDown } from "../../animations/element-animations";
 import { useAnimation, trigger, transition } from '@angular/animations';
 
 @Component({
   templateUrl: './page-one.component.html',
   styleUrls: ['./page-one.component.scss'],
   animations: [
-    trigger("fadeUp", [transition("* => *", useAnimation(fadeUp))])
+    trigger("fadeUp", [
+      transition(":enter", useAnimation(fadeUp)),
+      transition(":leave", useAnimation(fadeDown))
+
+    ])
   ]
 })
 export class PageOneComponent implements OnInit {
@@ -16,6 +20,8 @@ export class PageOneComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  @HostBinding('@fadeUp') private animate = true;
 
   triggerAnimation() {
     this.isActive = this.isActive === "active" ? "inActive" : "active";
